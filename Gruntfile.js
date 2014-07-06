e33d50d7a12d422f3c272c73fe47e81ec3cc1332
George McIntosh <george@georgecodes.com>
2014-07-06 12:30:30 +0100

    adding watch task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,21 @@ module.exports = function(grunt) {
                     'web-app/css/main.css': 'grails-app/assets/stylesheets/scss/main.scss'
                 }
             }
+        },
+
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: 'grails-app/assets/stylesheets/scss/**/*.scss',
+                tasks: ['sass']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('build', ['sass']);
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build','watch']);
 }
