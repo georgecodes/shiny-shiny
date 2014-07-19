db1b687b7a74dc580b1888f7cc9505a6476456b9
George McIntosh <george@georgecodes.com>
2014-07-19 09:25:48 +0100

    Updated events.groovy to prevent grunt processes hanging around after tests run

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,5 +1,16 @@
 eventCompileStart = { msg ->
     def process = "grunt".execute()
     process.consumeProcessOutput(System.out, System.err)
+    Thread shutdown = new Thread(
+            new Runnable() {
+                @Override
+                void run () {
+                    process.destroy()
+                }
+            }
+    )
+
+    Runtime.runtime.addShutdownHook(shutdown)
+
 }
 
